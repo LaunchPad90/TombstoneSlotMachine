8626663809e2fc60395cbeaa034a8ea345b11804
Jacob Hickey <HIckey@Jacobs-MacBook-Air.local>
2020-02-11 06:33:42 -0700

    update win logic and spin button

--- main.js
+++ main.js
@@ -2,8 +2,7 @@
 const images = [
     { value: 0, name: 'wyatt', payOut: 1000 },
     { value: 1, name: 'morgan', payOut: 2500 },
-    { value: 2, name: 'virgil', payOut: 3000 },
-    { value: 3, name: 'doc', payout: 5000 }
+    { value: 2, name: 'doc', payout: 5000 }
 ];
 
 // const probability = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3];  
@@ -29,16 +28,16 @@ const rightReel = document.getElementById('right-reel');
 /*----- event listeners -----*/
 spinBtn.addEventListener('click', function (e) {
     // console.log(e.target);
-    spin();
+    function spin() {
+        let rndIdx = Math.floor(Math.random() * images.length);
+        // console.log(rndIdx)
+    }
     setTimeout(grabImg1, 1000);
     setTimeout(grabImg2, 2000);
     setTimeout(grabImg3, 3000);
 })
 /*----- functions -----*/
-function spin() {
-    let rndIdx = Math.floor(Math.random() * images.length);
-    console.log(rndIdx)
-}
+
 
 function grabImg1() {
     let rndIdx = Math.floor(Math.random() * images.length);
@@ -48,12 +47,9 @@ function grabImg1() {
             p.innerText = images[i].name;
             leftReel.appendChild(p);
             console.log(p);
-            console.log(images[i].name)
+            console.log(images[i].value)
         }
     }
-
-
-    console.log(rndIdx)
 }
 
 function grabImg2() {
@@ -64,7 +60,7 @@ function grabImg2() {
             p.innerText = images[i].name;
             middleReel.appendChild(p);
             console.log(p);
-            console.log(images[i].name)
+            console.log(images[i].value)
         }
     }
 }
@@ -77,17 +73,18 @@ function grabImg3() {
             p.innerText = images[i].name;
             rightReel.appendChild(p);
             console.log(p);
-            console.log(images[i].name)
+            console.log(images[i].value)
         }
     }
 
     function checkWinner() {
         if (leftReel.innerHTML === middleReel.innerHTML && leftReel.innerHTML === rightReel.innerHTML) {
-
+            // console.log('winner')
         }
     }
+    checkWinner();
 
-    function updateMsg(msg) {
+    function updateMsg() {
         msg.appendChild('#top-bar')
     }
 
