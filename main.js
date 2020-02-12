c3e1618eb4e7890c0ba3789ebf32a900377b932f
Jacob Hickey <HIckey@Jacobs-MacBook-Air.local>
2020-02-11 21:19:18 -0700

    update player money

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
 /*----- constants -----*/
 const images = [
     { value: 0, name: 'wyatt', payOut: 10 },
-    { value: 1, name: 'morgan', payOut: 25 },
-    { value: 2, name: 'doc', payout: 50 }
+    // { value: 1, name: 'morgan', payOut: 25 },
+    // { value: 2, name: 'doc', payout: 50 }
 ];
 
-// const probability = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3];  
-// console.log(probability)
-
 /*----- app's state (variables) -----*/
-
 let betHolder = [];
-
-
 /*----- cached element references -----*/
-const msg = document.getElementById('top-bar');
 
 const spinBtn = document.getElementById('spinBtn');
 
@@ -24,25 +17,21 @@ const middleReel = document.getElementById('middle-reel');
 
 const rightReel = document.getElementById('right-reel');
 
-const betBtn = document.getElementById('betBtn');
-console.log(betBtn)
-
-const bet = document.querySelector('input');
-console.log(bet.innerText);
+const bet = document.querySelector('input')
+let newBet;
 
 let playerMoney = document.getElementById('player-money');
-console.log(playerMoney)
-
+let money = parseInt(playerMoney.innerText)
 
 
 /*----- event listeners -----*/
 spinBtn.addEventListener('click', function (e) {
-    // console.log(e.target);
     if (bet.value === '') {
         alert('Place a bet first!')
         return;
     }
-    if (bet.value > parseInt(playerMoney.innerText)) {
+
+    if (bet.value > playerMoney.innerText) {
         alert('you broke');
         return;
     }
@@ -50,21 +39,8 @@ spinBtn.addEventListener('click', function (e) {
     setTimeout(grabImg1, 1000);
     setTimeout(grabImg2, 2000);
     setTimeout(grabImg3, 3000);
-})
-
-betBtn.addEventListener('click', function (e) {
-    console.log(bet.value);
-    betHolder.push(bet.value);
-    spinBtn
-})
+});
 /*----- functions -----*/
-function placeBet() {
-    if (parseInt(bet.value) <= parseInt(playerMoney.innerText)) {
-        playerMoney.innerText = parseInt(playerMoney.innerText) - parseInt(bet.value);
-    }
-    console.log(parseInt(bet.value));
-    console.log(parseInt(playerMoney.innerText))
-}
 
 function grabImg1() {
     let rndIdx = Math.floor(Math.random() * images.length);
@@ -105,10 +81,12 @@ function grabImg3() {
     }
 
     function checkWinner() {
+        newBet = parseInt(bet.value);
         if (leftReel.innerText === middleReel.innerText && leftReel.innerText === rightReel.innerText) {
-            return parseInt(playerMoney.innerText += parseInt(bet.value * 2));
+            console.log(playerMoney)
+            return playerMoney.innerText = (money + newBet);
         } else {
-            parseInt(playerMoney.innerText -= parseInt(bet.value));
+            return playerMoney.innerText = (money - newBet);
         }
     }
     checkWinner();
